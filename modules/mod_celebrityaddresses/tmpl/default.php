dd1ef6476430376fde84b40fc67a660ef9a579df
Craig Baylis <craig.baylis@tcmsvc.com>
2011-12-13 19:29:49 +0530

    Address details on gavicktab

--- modules/mod_celebrityaddresses/tmpl/default.php
+++ modules/mod_celebrityaddresses/tmpl/default.php
@@ -9,43 +9,9 @@ $cid = JRequest::getInt('cid');
 $i = 1;
 $user = JFactory::getUser();
 ?>
-           <!-- <div id="gavickModule">
-				<ul id="gModule">
-					<li><a href="#">Postal Addresses</a></li>
-					<li><a href="#">Email Addresses</a></li>
-					<li><a href="#">Websites</a></li>
-				</ul>
-				<ul id="nextButtons">
-					<li class="nextCircle"><a href="#"></a></li>
-					<li class="nextCircle"><a href="#"></a></li>
-					<li class="nextCircle"><a href="#"></a></li>
-					<li id="backwardsArrow"><a href="#"></a></li>
-					<li id="forwardArrow"><a href="#"></a></li>
-				</ul>
-			</div>-->
-   			<div class="moduletable">
-			<div>
-	   			<div class="moduletable_content">
-					<div class="gk_tab gk_tab-style1 clearfix-tabs" id="tabmix1">
-                   			<div class="gk_tab_wrap-style1 clearfix-tabs" style="">
-                            <ul class="gk_tab_ul-style1">
-                                <li id="tabmix1_tab_1"><span>Postal Addresses</span></li>
-                                <li id="tabmix1_tab_2"><span>Email Addresses</span></li>
-                                <li id="tabmix1_tab_3"><span>Websites </span></li>
-					        </ul>
-                             <ul id="nextButtons">
-                        		<li class="nextCircle"><a href="#"></a></li>
-                                <li class="nextCircle"><a href="#"></a></li>
-                                <li class="nextCircle"><a href="#"></a></li>
-                                <li id="backwardsArrow"><a href="#"></a></li>
-                                <li id="forwardArrow"><a href="#"></a></li>
-						    </ul>
-							<div class="gk_tab_container0-style1">  <!--style="height: 100px;"-->
-						        <div class="gk_tab_container1-style1 clearfix-tabs"><!--style="height: 100px;"-->
-						            <div class="gk_tab_container2-style1 clearfix-tabs">
-										<div class="gk_tab_item-style1"><!--style="height: 100px;"-->
+ 
 											<?php if ($addresses) : ?>
-                                            <?php foreach ($addresses AS $key => $address) : ?>
+                                            <?php //foreach ($addresses AS $key => $address) : ?>
                                             <div class="individualAddress">
                                               <ul>
                                                 <li class="addressTitle"><?php echo JText::_('Address').''.$i ?></li>
@@ -58,51 +24,7 @@ $user = JFactory::getUser();
                                         </ul>
 										<!--<div class="gk_tab_item_space"></div>-->
 										</div>
-										<div class="gk_tab_item-style1" style="height: 100px;">
-											<div class="gk_tab_item_space">
-                                            <!--Place Email address code here-->
-                                            <!--<div class="individualAddress">
-                                              <ul>
-                                               <li class="addressTitle">Address2</li>
-                                                <li class="viewDetail"><a href="#"></a></li>
-                                                <li class="addressLine1">A Train</li>
-                                                <li class="addressLine2">PO B ****</li>
-                                                <li class="city_state_zip">Oakland, CA 94604</li>
-                                                <li class="submission">Submitted by: <a href="#">01.12.2011</a></li>
-                                                <li class="success"></li>
-                                              </ul>
-										   </div>-->
-										   </div>
-										</div>
-										<div class="gk_tab_item-style1" style="height: 100px;">
-											<div class="gk_tab_item_space">
-                                             <!-- Place Websites address code here-->
-                                              <!--<div class="individualAddress">
-                                               <ul>
-                                                <li class="addressTitle">Address3</li>
-                                                <li class="viewDetail"><a href="#"></a></li>
-                                                <li class="addressLine1">A Train</li>
-                                                <li class="addressLine2">PO B ****</li>
-                                                <li class="city_state_zip">Oakland, CA 94604</li>
-                                                <li class="submission">Submitted by: <a href="#">01.12.2011</a></li>
-                                                <li class="success"></li>
-                                               </ul>
-										      </div>-->
-                                            </div>
-										</div>
-                                  </div>
-                               </div>
-                        <div class="clearfix-tabs"></div>
-                        <div class="gk_tab_button_next-style1"></div>
-                        <div class="gk_tab_button_prev-style1"></div>
-    						</div>
-						</div>
-					</div>
-
-				</div>
-			</div>
-			</div>
-          </div>
+										
 
 <?php /*?><div class="addressList">
 <?php if ($addresses) : ?>
