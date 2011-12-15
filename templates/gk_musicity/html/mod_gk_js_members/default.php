945e9e55b559f5bfe3bb9591fa6aac380ed7d2fd
Craig Baylis <craig.baylis@tcmsvc.com>
2011-12-15 20:22:26 +0530

    Celebrity address details

---
+++ templates/gk_musicity/html/mod_gk_js_members/default.php
@@ -0,0 +1,292 @@
+<?php
+
+
+
+/**
+
+* Gavick GK JS Members - layout
+
+* @package Joomla!
+
+* @Copyright (C) 2009 Gavick.com
+
+* @ All rights reserved
+
+* @ Joomla! is Free Software
+
+* @ Released under GNU/GPL License : http://www.gnu.org/copyleft/gpl.html
+
+* @version $Revision: 1.0.1 $
+
+**/
+
+
+
+// no direct access
+
+defined('_JEXEC') or die('Restricted access');
+
+
+
+?>
+<div class="gk_js_members_main" id="<?php echo $this->config['module_unique_id']; ?>" style="width:<?php echo $this->config['module_width']; ?>px;">
+
+	<div class="gk_js_tabs">
+
+		<div class="gk_js_tabs_wrap">
+
+			<div class="gk_js_tab"><span><?php echo JText::_('POSTAL ADDRESSES'); ?></span></div>
+
+			<div class="gk_js_tab"><span><?php echo JText::_('EMAIL ADDRESSES'); ?></span></div>
+
+			<div class="gk_js_tab"><span><?php echo JText::_('WEBSITES'); ?></span></div>
+
+		</div>
+
+	</div>	
+
+<div>
+</div>
+
+	<div class="gk_js_content" style="width:<?php echo $this->config['module_width']; ?>px;">
+
+		<div class="gk_js_content_wrap">
+
+		<?php 
+
+			
+
+			$keys = array_keys($this->content);
+			for($i = 0; $i < count($keys); $i++) :
+		?>
+
+			<div class="gk_js_members">
+
+				<div class="gk_js_members_scroll1" style="width:<?php echo $this->config['module_width']; ?>px;">		
+
+					<div class="gk_js_members_scroll2">		
+
+						<?php 
+
+							$member_counter = 0;
+
+							$total = count($this->content[$keys[$i]]);
+							if($total==0){
+							echo "No Information found.<br><br><br><br>";}else{
+$j=1;
+
+							foreach($this->content[$keys[$i]] as $member) : 
+							
+
+						?>			
+
+						
+
+							<?php if($member_counter % ($this->config['rows'] * $this->config['cols']) == 0) : ?>
+
+							<div class="gk_js_members_wrap" style="width:<?php echo $this->config['module_width']; ?>px;">
+
+							<?php endif; ?>
+							<div class="gk_js_member" style="width:<?php echo floor(($this->config['module_width']) / ($this->config['cols'])); ?>px;">
+								<div class="gk_js_member_wrap">
+
+									
+									
+									<?php /*?><?php if( $this->config['show_avatar'] ) : ?>
+
+									<a href="index.php?option=com_community&amp;view=profile&amp;userid=<?php echo $member->id; ?>" class="gk_js_avatar">
+
+										<?php 
+
+											$user =& CFactory::getUser($member->id);
+
+											$avatarUrl = ($this->config['avatar_size'] == 'thumb') ? $user->getThumbAvatar() : $user->getAvatar();
+										?>
+										<img src="<?php echo $avatarUrl; ?>" height="<?php echo $this->config['avatar_height']; ?>" alt="<?php echo $member->name; ?>" />
+
+									</a>
+
+									<?php endif; ?>
+<?php */?>
+									   <div class="individualAddress" <?php if($member->email || $member->url){?>style="height:70px"<?php }?>>
+                                              <ul>
+                                                <li class="addressTitle"><?php if($member->company){ echo JText::_('Address').''. $j++; } if($member->email){ echo JText::_('Email Address').''. $j++;}if($member->url){ echo JText::_('Web Address').''. $j++;}?></li>
+                                                 <?php if($member->company){
+													 ?>
+                                                <li class="viewDetail"><a href="#"></a></li>
+                                                <?php } ?>
+                                                <li class="addressLine1"><?php //echo ($member->company)? $member->company: $member->name
+										if($member->company){echo $member->company;} if($member->email){ echo $member->email;} if($member->url){ echo $member->url;} ?>		
+												 </li>
+                                                 <?php if($member->company){
+													 ?>
+                                                <li class="addressLine2"><?php
+												$user =& CFactory::getUser($member->id);
+												echo ($user->get('id')) ? $member->line_1 : substr($member->line_1,0,4).' ****' ?></li>
+                                                <li class="city_state_zip"><?php echo $member->city.', '.$member->state.' '.$member->zipcode ?></li>
+                                                <li class="submission">Submitted by: <a href="#"><?php echo $member->date ?></a></li>
+                                      <li class="success"><img src="<?php echo JURI::base();?>templates/gk_musicity/images/style4/success_check.png" alt="success" title="success"/>Success  <img src="<?php echo JURI::base();?>templates/gk_musicity/images/style4/success_green.png" alt="successNumber" title="successNumber"><a class="green" href="#"><?php echo (!empty($successCounts[$key]->total_success)) ? $successCounts[$key]->total_success : '0' ?></a><a class="red" href="#"><img src="<?php echo JURI::base();?>templates/gk_musicity/images/style4/unsuccess_red.png" alt="unsuccessNumber" title="unsuccessNumber"/><?php echo (!empty($returnedCounts[$key]->total_returned)) ? $returnedCounts[$key]->total_returned : '0' ?></a></li>  
+                                      <?php
+												 }
+									  ?>         
+                                        </ul>
+										</div>       
+
+									<?php /*?><?php if( $this->config['show_name'] ) : ?>
+
+									<div class="gk_js_member_name">
+
+										<span><?php if($member->company){
+											 echo $member->company;
+											 
+									      }
+										 if($member->email){ echo $member->email;}
+										 if($member->url){ echo $member->url;}; ?></span>
+
+									</div>
+
+									<?php endif; ?>
+
+									
+
+									<?php if( $this->config['show_since'] ) : ?>
+
+									<div class="gk_js_since">
+
+										<span><?php echo JText::_('MEMBER_SINCE'); ?></span>
+
+										<span><?php echo date('d.m.y',strtotime($member->registerDate)); ?></span>
+
+									</div>	
+
+									<?php endif; ?>
+
+									
+
+									<?php if( $this->config['show_lastonline'] ) : ?>
+
+									<div class="gk_js_lastonline">
+
+										<span><?php echo JText::_('LAST_ONLINE'); ?></span>
+
+										<span><?php echo (strtotime($member->lastvisitDate) == 0) ? JText::_('NEVER') : date('d.m.y h:i',strtotime($member->lastvisitDate)); ?></span>
+
+									</div>
+
+									<?php endif; ?>
+
+									
+
+									<?php if( $this->config['show_views'] ) : ?>
+
+									<div class="gk_js_profileviews">
+
+										<span><?php echo JText::_('PROFILE_VIEWS'); ?></span>
+										<span><?php echo $member->_view; ?></span>
+
+									</div>
+
+									<?php endif; ?><?php */?>
+
+								</div>
+
+							</div>
+
+							
+
+						<?php 
+
+							
+
+							$member_counter++;
+
+							if($member_counter % $this->config['cols'] == 0) echo '<div class="clearfix"></div>';
+
+							
+
+						?>
+
+						
+
+						<?php 
+
+								if(
+
+									(($member_counter % ($this->config['rows'] * $this->config['cols'])) == 0)
+
+									||
+
+									($member_counter == $total)
+
+								) : 
+
+						?>
+
+						
+
+						</div>
+
+						
+
+						<?php 
+
+							endif; 
+
+							endforeach; 
+							}
+						?>
+
+					</div>
+
+				</div>
+
+				<?php if($this->config['pages'] > 1 && $total > ($this->config['rows'] * $this->config['cols'])): ?>
+
+				<div class="gk_js_interface">
+
+					<div class="clearfix">
+
+						<?php for($x = 0; $x < ceil($total / ($this->config['rows'] * $this->config['cols']));$x++) : ?>
+
+						<span class="gk_js_page"><?php echo $x+1; ?></span>
+
+						<?php endfor; ?>
+						<span class="gk_js_prev">&laquo;</span>
+
+						<span class="gk_js_next">&raquo;</span>
+
+					</div>
+
+				</div>	
+
+                <?php endif; ?>		
+
+			</div>
+
+
+
+
+
+			<?php endfor; ?>
+
+			<div class="gk_js_overlay"></div>
+
+		</div>	
+
+	</div>		
+
+</div>
+
+<script type="text/javascript">
+
+	try{$Gavick;}catch(e){$Gavick = {};}
+
+	$Gavick["gk_js_members-<?php echo $this->config['module_unique_id'];?>"] = {
+
+		"animationType" : Fx.Transitions.<?php echo $this->config['animation'];?>,
+
+		"animationSpeed" : <?php echo $this->config['anim_speed'];?>
+
+	};
+
+</script>
