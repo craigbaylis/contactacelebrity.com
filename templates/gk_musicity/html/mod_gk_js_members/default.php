9306da33e51755ebe194da38165f1e4a399c6c56
Craig Baylis <craig.baylis@tcmsvc.com>
2011-12-20 20:32:17 +0530

    craig

--- templates/gk_musicity/html/mod_gk_js_members/default.php
+++ templates/gk_musicity/html/mod_gk_js_members/default.php
@@ -80,9 +80,16 @@ $getuid =$_SESSION[__default][user]->id;
 <a href="<?php echo JURI::base();?>index.php?option=com_celebrity&task=add&controller=address&cid=<?php echo $_GET['cid'];?>&Itemid=61"><img src="<?php echo JURI::base();?>templates/gk_musicity/images/style4/addNewAddress_box.png" alt="addNewAddress_box" title="Have another address? Add it here!" width="114" height="133" /></a>
 </div>
 <?php } else {?>
-                        <div class="noRightBorder" id="addAnotherAddress">
-<a href="<?php echo JURI::base();?>index.php?option=com_celebrity&task=add&controller=address&cid=<?php echo $_GET['cid'];?>&Itemid=61"><img src="<?php echo JURI::base();?>templates/gk_musicity/images/style4/addNewAddress_box.png" alt="addNewAddress_box" title="Have another address? Add it here!" width="114" height="133" /></a>
-</div>
+                <?php if($getuid==false){ ?>
+					<div class="noRightBorder" id="addAnotherAddress">
+<a href="<?php echo JRoute::_('index.php?option=com_user&view=login') ?>" class="general_login"><img src="<?php echo JURI::base();?>templates/gk_musicity/images/style4/addNewAddress_box.png" alt="addNewAddress_box" title="Have another address? Add it here!" width="114" height="133" /></a>
+					</div>
+
+				<?php }else {?>
+                		<div class="noRightBorder" id="addAnotherAddress">
+                		<a href="<?php echo JURI::base();?>index.php?option=com_celebrity&task=add&controller=address&cid=<?php echo $_GET['cid'];?>&Itemid=61"><img src="<?php echo JURI::base();?>templates/gk_musicity/images/style4/addNewAddress_box.png" alt="addNewAddress_box" title="Have another address? Add it here!" width="114" height="133" /></a>
+                        </div>
+                        <?php }?>
 <?php }?>
 							
                           <!-- div.individualAddress close -->
@@ -93,14 +100,9 @@ $j=1;
 
 							foreach($this->content[$keys[$i]] as $member) : 
 							
-						?>			
-
-						
-
+						?>	
 							<?php if($member_counter % ($this->config['rows'] * $this->config['cols']) == 0) : ?>
-
 							<div class="gk_js_members_wrap" style="width:<?php echo $this->config['module_width']; ?>px;">
-
 							<?php endif; ?>
 							<div class="gk_js_member" style="width:<?php echo floor(($this->config['module_width']) / ($this->config['cols'])); ?>px; border-bottom:1px solid #000;">
 								<div class="gk_js_member_wrap">
@@ -127,7 +129,7 @@ $j=1;
 									   <div class="individualAddress" <?php /*?><?php if($member->email || $member->url){?>style="height:70px"<?php }?><?php */?>>
                                               <ul>
                                               <?php if($member->name){?>
-                                                <li class="addressTitle"><?php echo JText::_('Address').''. $j++;?>
+                                                <li class="addressTitle" style="margin-left:-7px;"><?php echo JText::_('Address').''. $j++;?>
                                                 </li>
                                                 <?php }?>
                                                  <?php if($member->email){?>
@@ -144,7 +146,7 @@ $j=1;
                                                 <li class="viewDetail"><a href="#"></a></li>
                                                 <?php } ?>
                                                <?php if($member->name){?>
-                                                <li class="addressLine1">
+                                                <li class="addressLine1" style="width:520px; float:left;margin-left:-7px;">
 												<?php echo ($member->company=="")?$member->name:$member->company;?>
                                                  </li>
                                                  <?php }?>
@@ -173,13 +175,12 @@ $j=1;
                                                  <?php }?>
                                                  <?php if($member->name){
 													 ?>
-                                                <li class="addressLine2"><?php
+                                                <li class="addressLine2" style="width:520px; float:left;margin-left:-7px;"><?php
 												$user =& CFactory::getUser($member->id);
-												echo $getuid;
 												echo ($getuid!='') ? $member->line_1 : str_replace(substr($member->line_1,5),"********",$member->line_1); ?></li>
-                                                <li class="city_state_zip"><?php echo $member->city.', '.$member->state.' '.$member->zipcode ?></li>
-                                                <li class="submission">Submitted by: <a href="index.php?option=com_community&view=profile&userid=<?php echo $member->created_by_uid;?>&Itemid=41"><?php echo $member->username;?></a></li>
-                                      <li class="success"><img src="<?php echo JURI::base();?>templates/gk_musicity/images/style4/success_check.png" alt="success" title="success"/>Success  <img src="<?php echo JURI::base();?>templates/gk_musicity/images/style4/success_green.png" alt="successNumber" title="successNumber"><a class="green" href="#"><?php echo (!empty($successCounts[$key]->total_success)) ? $successCounts[$key]->total_success : '0' ?></a><a class="red" href="#"><img src="<?php echo JURI::base();?>templates/gk_musicity/images/style4/unsuccess_red.png" alt="unsuccessNumber" title="unsuccessNumber"/><?php echo (!empty($returnedCounts[$key]->total_returned)) ? $returnedCounts[$key]->total_returned : '0' ?></a></li>  
+                                                <li class="city_state_zip" style="width:520px; float:left;margin-left:-7px;"><?php echo substr($member->city,0,10).', '.$member->state.' '.substr($member->zipcode,0,6); ?></li>
+                                                <li class="submission" style="width:520px; float:left;margin-left:-7px;">Submitted by: <a href="index.php?option=com_community&view=profile&userid=<?php echo $member->created_by_uid;?>&Itemid=41"><?php echo $member->username;?></a></li>
+                                      <li class="success" style="width:520px; float:left;margin-left:-7px;"><img src="<?php echo JURI::base();?>templates/gk_musicity/images/style4/success_check.png" alt="success" title="success"/>Success  <img src="<?php echo JURI::base();?>templates/gk_musicity/images/style4/success_green.png" alt="successNumber" title="successNumber"><a class="green" href="#"><?php echo (!empty($successCounts[$key]->total_success)) ? $successCounts[$key]->total_success : '0' ?></a><a class="red" href="#"><img src="<?php echo JURI::base();?>templates/gk_musicity/images/style4/unsuccess_red.png" alt="unsuccessNumber" title="unsuccessNumber"/><?php echo (!empty($returnedCounts[$key]->total_returned)) ? $returnedCounts[$key]->total_returned : '0' ?></a></li>  
                                       <?php
 										}
 									  ?>
@@ -252,14 +253,10 @@ $j=1;
 		<?php 
 							$member_counter++;
 							if($member_counter % $this->config['cols'] == 0) echo '<div class="clearfix"></div>';
-
-				
-						?>
-
+							?>
 						<?php
 						//check a session user.
 						$getuser =$getuid;
-						
 						//personal address
 						 if($member->name && $member_counter==$total){?>
                         <div class="noRightBorder" id="addAnotherAddress">
@@ -273,11 +270,16 @@ $j=1;
 <?php }?>
 <?php
 //email address
+
  if($member->email && $member_counter==$total){?>
                         <div class="noRightBorder" id="addAnotherAddress">
-                          <?php if($getuser == "0"){?>
+                          <?php 
+						  $getuseremail =$_SESSION[__default][user]->id;
+						 
+						  if($getuseremail == "0"){?>
                         <a href="<?php echo JRoute::_('index.php?option=com_user&view=login') ?>" class="general_login"><img src="<?php echo JURI::base();?>templates/gk_musicity/images/style4/addNewAddress_box.png" alt="addNewAddress_box" title="Have another address? Add it here!" width="114" height="133" /></a>
-                        <?php } else {?>
+                        <?php } 
+						else{?>
 <a href="<?php echo JURI::base();?>index.php?option=com_celebrity&task=add&controller=address&cid=<?php echo $_GET['cid'];?>&Itemid=61"><img src="<?php echo JURI::base();?>templates/gk_musicity/images/style4/addNewAddress_box.png" alt="addNewAddress_box" title="Have another address? Add it here!" width="113" height="133" /></a>
 <?php }?>
 </div><?php }?>
